de06f53975f1b98b5d26e1d0925069770822c525
kaytopod <kaytopod0119@gmail.com>
2024-09-05 19:33:43 +0900

    マウスの方向に向けて投げる

--- Assets/Script/igaguriGenerator.cs
+++ Assets/Script/igaguriGenerator.cs
@@ -13,7 +13,12 @@ public class igaguriGenerator : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             GameObject igaguri = Instantiate(igaguriPrafab);
-            igaguri.GetComponent<igaguriController>().Shoot(new Vector3(0, 200, 2000));
+//            igaguri.GetComponent<igaguriController>().Shoot(new Vector3(0, 200, 2000));
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 worldDir = ray.direction;
+
+            igaguri.GetComponent<igaguriController>().Shoot(worldDir.normalized*2000);
 
         }
     }
